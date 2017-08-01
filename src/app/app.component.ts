d4b2a2165e37d88954a91c6f501c3116c2582f6b
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-02 00:06:30 +0300

    populating multiple articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular2', 'http://angular.io', 2),
+      new Article('Reddit', 'http://reddit.com', 3),
+      new Article('RaghibM', 'http://raghibm.com', 10),
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
